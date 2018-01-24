3e0633b45cf1a2b21b4f48f814b1cb5ac48ecbbe
xu_bohan <xu_bohan@126.com>
2018-01-24 18:38:35 +0800

    添加jdk的鼠标控制

--- src/main/java/org/naozi/OnmyojiX/task/opencv/Demo.java
+++ src/main/java/org/naozi/OnmyojiX/task/opencv/Demo.java
@@ -4,19 +4,42 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.util.Date;
+
 /**
  * @author lenovo
  * @date 2018/1/12
  */
 public class Demo {
+
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        //注意程序运行的时候需要在VM option添加该行 指明opencv的dll文件所在路径
-        //-Djava.library.path=$PROJECT_DIR$\opencv\x64
+        // 注意程序运行的时候需要在VM option添加该行 指明opencv的dll文件所在路径
+        // -Djava.library.path=$PROJECT_DIR$\opencv\x64
     }
 
     public static void main(String[] args) {
-        Mat mat = Imgcodecs.imread("C:/opencv.png");
+        // 注意图片路径中不能出现中文
+        System.out.println(System.currentTimeMillis());
+        Mat mat = Imgcodecs.imread("C:\\Users\\lenovo\\Desktop\\riri.jpg");
+        System.out.println(System.currentTimeMillis());
         System.out.println(mat);
+
+        try
+        {
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            Robot robot = new Robot();
+            robot.mouseMove(screenSize.width - 300, 5);
+            robot.delay(1000);
+            robot.mousePress(InputEvent.BUTTON1_MASK);
+            robot.delay(300);
+            robot.mouseRelease(InputEvent.BUTTON1_MASK);
+        }
+        catch (AWTException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
